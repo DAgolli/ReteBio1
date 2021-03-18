f0f24e9b4aad8f18356e33094b0c1cf20c5ca1bc
desag97 <72747697+desag97@users.noreply.github.com>
2021-03-18 14:38:09 +0100

    styles login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useHistory } from 'react-router-dom'
-import {login} from '../redux/actions/authAction'
+import { login } from '../redux/actions/authAction'
 import { useDispatch, useSelector } from 'react-redux'
+import { Grid, TextField, Paper, InputAdornment, IconButton } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import useStyles from '../styles/styles_login_register';
+import Logo from "../images/logo.png";
+//import ReactPlayer from 'react-player';
+
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+
 
 
 const Login = () => {
     const initialState = { email: '', password: '' }
     const [userData, setUserData] = useState(initialState)
-    const { email, password } = userData
-
-    const [typePass, setTypePass] = useState(false)
-
+    // ikona e show password
+const [showPassword, setShowPassword] = useState(false);
+const handleClickShowPassword = () => setShowPassword(!showPassword);
+const handleMouseDownPassword = () => setShowPassword(!showPassword);
+    // const [typePass, setTypePass] = useState(false)
+    const classes = useStyles();
     const { auth } = useSelector(state => state)
     const dispatch = useDispatch()
     const history = useHistory()
@@ -19,51 +30,70 @@ const Login = () => {
         if(auth.token) history.push("/")
     }, [auth.token, history])
 
-    const handleChangeInput = e => {
-        const {name, value} = e.target
-        setUserData({...userData, [name]:value})
-    }
-
     const handleSubmit = e => {
         e.preventDefault()
         dispatch(login(userData))
     }
 
     return (
-        <div className="auth_page">
-            <form onSubmit={handleSubmit}>
-                <h3 className="text-uppercase text-center mb-4">ReteBio</h3>
-                <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" 
-                    aria-describedby="emailHelp" onChange={handleChangeInput} value={email} name="email"/>
-                    
-                    <small id="emailHelp" className="form-text text-muted">
-                        We'll never share your email with anyone else.
-                    </small>
+        <div>
+            <video src='/videos/video-5.mp4' className={classes.video} autoPlay loop muted />
+            
+            <form noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Grid className={classes.grid} container justify="center">
+                <Grid item xs={12} sm={6}>
+                  {/* <img className={classes.imgLoad} src={Load} alt="Load" /> */}
+                
+                <Paper elevation={10} className={classes.paper}>
+                <img className={classes.imgLogin} src={Logo} alt="Logo" />  
+                <div>
+                   <TextField
+                   className={classes.textField} 
+                   label="Email" 
+                   variant="outlined"
+                   type="email" 
+                   fullWidth 
+                   value={userData.email} 
+                   onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+
+                   />
+
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <div className="pass">
-                        <input type={ typePass ? "text" : "password"} 
-                        className="form-control" id="exampleInputPassword1"
-                        onChange={handleChangeInput} value={password} name="password" />
-                        <small onClick={() => setTypePass(!typePass)}>
-                            {typePass ? 'Hide' : 'Show'}
-                        </small>
-                    </div>
-                    
+                <div>
+                    <TextField 
+                    label="Password"
+                    variant="outlined"
+                    type={showPassword ? "text" : "password"}
+                    fullWidth
+                    value={userData.password}
+                    onChange={(e) => setUserData({...userData, password: e.target.value })}
+                    InputProps={{ // <-- This is where the toggle button is added.
+                    endAdornment: (
+                    <InputAdornment position="end">
+                    <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                    </InputAdornment>
+                    )
+                }}
+                    />
                 </div>
-                
-                <button type="submit" className="btn btn-dark w-100"
-                disabled={email && password ? false : true}>
-                    Login
-                </button>
+
+
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Login</Button>
 
                 <p className="my-2">
                     You dont have an account? <Link to="/register" style={{color: "crimson"}} >Register Now</Link>
                 </p>
+                </Paper>
+                </Grid>
+                </Grid>
+
             </form>
         </div>
     )
---
+++ client/src/styles/styles_login_register.js
@@ -0,0 +1,70 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1.5),
+        },
+    },
+
+    // textField: {
+    //     marginLeft:"50%",
+
+    // },
+
+    imgLogin: {
+        display: 'block',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        width: '30%',
+        height: '30%',
+        objectFit: 'contain',
+    },
+
+    // imgLoad: {
+    //     display: 'block',
+    //     marginRight: 'auto',
+    //     marginLeft: 'auto',
+    //     width: '100%',
+    //     height: '100%',
+    //     objectFit: 'contain',
+    // },
+
+    // video: {
+    //     position: "absolute",
+    //     width: "100%",
+    //     left: "50%",
+    //     top: "50%",
+    //     height: "100%",
+    //     objectFit: "cover",
+    //     transform: "translate(-50%, -50%)",
+    //     zIndex: "-1",
+    // },
+
+    // videoRetebio: {
+    //     display: "flex",
+    //     marginRight: 'auto',
+    //     marginLeft: 'true',
+        
+    // },
+
+    paper: {
+        padding: theme.spacing(2),
+    },
+
+    form: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+   
+    buttonSubmit: {
+        marginTop: 10,
+    },
+
+    // grid: {
+    //     minHeight: '100vh',
+    //     marginTop: 250,
+    // },
+}));
